7dc136206d963938ae0b0c98f3018b55a1d1b187
Eric Kenny <eric.kenny@ameritas.com>
2018-12-03 14:16:54 -0500

    fixed run script

--- resources/rundirsync.sh
+++ resources/rundirsync.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-echo off
-
-INSTALL_DIR=~./local/bin
+INSTALL_DIR=$HOME/.local/bin
 REMOTE_MOUNT_POINT=/Volumes/AUETK
 USER_ID=auetk
 LOG_FILE=/var/log/dirsync/dirsync.log
 CONFIG_FILE=dirsync.conf
+RUN_ID=$(uuidgen)
+
 
 cd "$INSTALL_DIR" 
 
-date >> "$LOG_FILE"
+echo "Run ($RUN_ID) started at $(date)" >> "$LOG_FILE"
 
 # if the remote SMB share hasn't been mounted yet, mount it.
-if [ ! -d "$REMOTE_MOUNT_POINT"]; then
+if [[ ! -d "$REMOTE_MOUNT_POINT" ]]; then
   echo "$REMOTE_MOUNT_POINT does not exist. Mounting network share" >> "$LOG_FILE"
   mkdir "$dREMOTE_MOUNT_POINT"
   mount_smbfs //$USER_ID@corporate.unificompanies.com/users/Cincinnati/$USER_ID $REMOTE_MOUNT_POINT
@@ -21,6 +21,6 @@ fi
 
 ./dirsync "$CONFIG_FILE" >> "$LOG_FILE"
 
-date >> "$LOG_FILE"
+echo "$RUN_ID completed at $(date)" >> "$LOG_FILE"
 
 
