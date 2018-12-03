c754f0e1ff2298140640d42ef3b7142f454b348d
Eric Kenny <eric.kenny@ameritas.com>
2018-12-03 10:49:08 -0500

    trying to handle non-mounted network share

--- resources/rundirsync.sh
+++ resources/rundirsync.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
+
 echo off
 
-cd ~/.local/bin
+INSTALL_DIR=~./local/bin
+REMOTE_MOUNT_POINT=/Volumes/AUETK
+USER_ID=auetk
+LOG_FILE=/var/log/dirsync/dirsync.log
+CONFIG_FILE=dirsync.conf
 
-date >> /var/log/dirsync/dirsync.log
-./dirsync dirsync.conf >> /var/log/dirsync/dirsync.log
-date >> /var/log/dirsync/dirsync.log
+cd "$INSTALL_DIR" 
+
+date >> "$LOG_FILE"
+
+# if the remote SMB share hasn't been mounted yet, mount it.
+if [ ! -d "$REMOTE_MOUNT_POINT"]; then
+  echo "$REMOTE_MOUNT_POINT does not exist. Mounting network share" >> "$LOG_FILE"
+  mkdir "$dREMOTE_MOUNT_POINT"
+  mount_smbfs //$USER_ID@corporate.unificompanies.com/users/Cincinnati/$USER_ID $REMOTE_MOUNT_POINT
+fi
+
+./dirsync "$CONFIG_FILE" >> "$LOG_FILE"
+
+date >> "$LOG_FILE"
 
 
