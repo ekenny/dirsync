05c50fb82fa1d0846bffb888a62d2a5f85920f88
Eric Kenny <eric.kenny@ameritas.com>
2018-12-04 09:13:15 -0500

    reverted to IO monad

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,16 +12,16 @@ import System.Posix.Types
 import System.FilePath.Posix
 
 
-syncConfiguredPaths :: MonadIO m => Maybe DirSyncConfig -> m Integer
+syncConfiguredPaths :: Maybe DirSyncConfig ->  IO Integer
 syncConfiguredPaths Nothing = do liftIO . print $ "Unable to read configuration"; return 0
 syncConfiguredPaths (Just (DirSyncConfig dms)) =
   do
     fileCounts <- mapM syncPathConfig dms
     return (sum fileCounts)
 
-syncPathConfig :: MonadIO m => DirMapping -> m Integer
+syncPathConfig :: DirMapping -> IO Integer
 syncPathConfig dm@DirMapping{source=src, destination=dest, ignore=ig} = do
-  fs <- liftIO.getFileStatus $ src
+  fs <- getFileStatus src
   if shouldSkip ig src
     then return 0
     else syncItem' (isDirectory fs) src dest (shouldSkip ig) 0
@@ -30,15 +30,13 @@ shouldSkip :: IgnoreList -> FilePath  -> Bool
 shouldSkip il f = fn `elem` il
   where fn = takeFileName f
 
-syncItem' :: MonadIO m => Bool -> FilePath -> FilePath -> (FilePath -> Bool) -> Integer -> m Integer
-syncItem' isDir src dest skip cnt | not isDir = do 
-                                              copyIfNewer src dest 
-                                              return (cnt + 1)
+syncItem' :: Bool -> FilePath -> FilePath -> (FilePath -> Bool) -> Integer -> IO Integer
+syncItem' isDir src dest skip cnt | not isDir = copyIfNewer src dest cnt 
                                   | isDir =
   do
-    liftIO . print $ (src,  dest)
-    liftIO $ createDirectoryIfMissing True dest {- True means create parents -} 
-    absPaths <- liftIO . getAbsoluteItems $ src
+    --print (src,  dest)
+    createDirectoryIfMissing True dest {- True means create parents -} 
+    absPaths <- getAbsoluteItems src
     syncDirectoryContent absPaths dest skip cnt
 
 getAbsoluteItems :: FilePath -> IO [FilePath]
@@ -46,23 +44,25 @@ getAbsoluteItems dir = do
     paths <- listDirectory dir
     return $ map (combine dir) paths
 
-syncDirectoryContent :: MonadIO m => [FilePath] -> FilePath -> (FilePath -> Bool) -> Integer -> m Integer
-syncDirectoryContent [] _ _ _ = return 0
+syncDirectoryContent :: [FilePath] -> FilePath -> (FilePath -> Bool) -> Integer -> IO Integer
+syncDirectoryContent [] _ _ c = return c
 syncDirectoryContent (fp:fps) dest skip cnt = do
-  fs <- liftIO.getFileStatus $ fp
+  fs <- getFileStatus fp
   newCnt <- if skip fp
-               then return 0
+               then return cnt
                else syncItem' (isDirectory fs) fp destFile skip cnt
   syncDirectoryContent fps dest skip newCnt
   where
     destFile = dest </> takeFileName fp
 
-copyIfNewer :: MonadIO m => FilePath -> FilePath -> m ()
-copyIfNewer s t = do
-  n <- liftIO $ sourceNewer s t
-  when n $ do
-      liftIO $ copyFile s t
-      liftIO.print $ ("copied", s, "->", t)
+copyIfNewer :: FilePath -> FilePath -> Integer -> IO Integer
+copyIfNewer s d cnt = do
+  newer <- sourceNewer s d
+  if newer then do
+      copyFile s d
+      print ("copied", s, "->", d)
+      return (cnt + 1)
+  else return cnt
   
 sourceNewer :: FilePath -> FilePath -> IO Bool
 sourceNewer src target = do
